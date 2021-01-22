8b499990c086c3da46695cd72089527a21491ce9
devWRM <mcwen@comcast.net>
2021-01-21 20:31:38 -0500

    nested routes to create a route to fetch all books for a specific library

--- backend-library/config/routes.rb
+++ backend-library/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :libraries
+  
+  resources :libraries do
+    resources :books end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
