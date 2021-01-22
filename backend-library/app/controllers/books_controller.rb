5232a05d9926caa389e474062cd51f811b76a75a
devWRM <mcwen@comcast.net>
2021-01-22 14:36:07 -0500

    refactored book's action methods to reflect nesting in the library

--- backend-library/app/controllers/books_controller.rb
+++ backend-library/app/controllers/books_controller.rb
@@ -1,21 +1,25 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :update, :destroy]
+  # before_action :set_book, only: [:show, :update, :destroy]
+  before_action :set_library
 
   # GET /books
   def index
-    @books = Book.all
+    # @books = Book.all
+    @books = @library.books
 
     render json: @books
   end
 
   # GET /books/1
   def show
+    @book = Book.find(params[:id])
     render json: @book
   end
 
   # POST /books
   def create
-    @book = Book.new(book_params)
+    # @book = Book.new(book_params)
+    @book = @library.books.new(book_params)
 
     if @book.save
       render json: @book, status: :created, location: @book
@@ -35,13 +39,21 @@ class BooksController < ApplicationController
 
   # DELETE /books/1
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
+
+    # ?? @library ??
+    render json: @book
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
+    # def set_book
+    #   @book = Book.find(params[:id])
+    # end
+
+    def set_library
+      @library = Library.find(params[:library_id])
     end
 
     # Only allow a trusted parameter "white list" through.
