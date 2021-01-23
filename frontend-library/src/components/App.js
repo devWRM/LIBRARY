7fec9f96b597eb9a1ae4709e9a2b42b792703684
devWRM <mcwen@comcast.net>
2021-01-22 19:45:04 -0500

    installed and imported installations to setup Redux in index File, created rootReducer file to pass it into createStore

--- frontend-library/src/components/App.js
+++ frontend-library/src/components/App.js
@@ -3,9 +3,6 @@ import './App.css';
 
 class App extends React.Component {
 
-  
-
-
   render() {
       return (
         <div className="App">
--- frontend-library/src/index.js
+++ frontend-library/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
 import './index.css';
 import App from './components/App';
+import rootReducer from './reducers/rootReducer';
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+        
+            <App />
+        
+  </Provider>,
+
   document.getElementById('root')
 );
 
