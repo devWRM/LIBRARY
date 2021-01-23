46375633f77208ede0cb7cfeffa297305f48dc45
devWRM <mcwen@comcast.net>
2021-01-23 10:03:37 -0500

    added the NEW_LIBRARY case & return

--- frontend-library/src/reducers/librariesReducer.js
+++ frontend-library/src/reducers/librariesReducer.js
@@ -2,6 +2,8 @@ export const librariesReducer = (state = [], action) => {
     switch(action.type) {
         case 'FETCH_LIBRARIES':
             return action.payload
+        case 'NEW_LIBRARY':
+            return [...state, action.payload]
         default:
             return state
     }
