7d32c8c3cd3c49deaeddc451edcf401083602d05
devWRM <mcwen@comcast.net>
2021-01-23 09:59:53 -0500

    imported in the Library form to display in browser

--- frontend-library/src/components/LibraryContainer.js
+++ frontend-library/src/components/LibraryContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 
 import { fetchLibraries } from '../actions/libraryActions.js';
+import LibraryForm from './LibraryForm.js';
 
 class LibraryContainer extends Component {
 
@@ -15,6 +16,7 @@ class LibraryContainer extends Component {
         return (
             <div>
                 LIBRARY CONTAINER
+                <LibraryForm />
             </div>
         )
 
