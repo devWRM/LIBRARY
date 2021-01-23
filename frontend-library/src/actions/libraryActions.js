793728a09641ca97b88076acb312d3f065c2e3d4
devWRM <mcwen@comcast.net>
2021-01-23 09:58:07 -0500

    created the newLibrary action and setup a console.log to see what data is returned

--- frontend-library/src/actions/libraryActions.js
+++ frontend-library/src/actions/libraryActions.js
@@ -9,3 +9,24 @@ export const fetchLibraries = () => {
 }
 
 
+export const newLibrary = libraryInput => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/libraries', {
+            method: 'POST',
+            body: JSON.stringify(libraryInput),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then(resp => resp.json())
+        .then(dataLibrary => console.log(dataLibrary))
+        // .then(dataLibrary => dispatch({ type: 'NEW_LIBRARY', payload: dataLibrary }))
+
+    }
+
+}
+
+
+
+
+
+
+
