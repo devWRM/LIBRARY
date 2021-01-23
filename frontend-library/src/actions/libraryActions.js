db4608c7f5cd032ff3b68da5e32ba1fe79936e19
devWRM <mcwen@comcast.net>
2021-01-23 08:32:30 -0500

    testing code, no changes

--- frontend-library/src/actions/libraryActions.js
+++ frontend-library/src/actions/libraryActions.js
@@ -1,6 +1,6 @@
 export const fetchLibraries = () => {
     return (dispatch) => {
-        fetch("http://localhost:3000/libraries")
+        fetch('http://localhost:3000/libraries')
         .then(resp => resp.json())
         .then(dataLibraries => console.log(dataLibraries))
 
