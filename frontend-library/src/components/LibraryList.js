ca12c4588161a2bc26125535344e588a7bc290b2
devWRM <mcwen@comcast.net>
2021-01-23 17:35:18 -0500

    coded Library List to list all Libraries

--- frontend-library/src/components/LibraryList.js
+++ frontend-library/src/components/LibraryList.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 
 function LibraryList(props) {
+
+    
+
     return (
         <div>
-            The Library List
+            THE LIBRARY LIST:<br></br>
+            click a name to see library details<br></br>
+            {props.libraries.map(library => <li key={library.id}>
+                {library.name}<br></br>
+                {library.street}<br></br>
+                {library.city}<br></br>
+                {library.state}<br></br>
+            </li>)}
+            
         </div>
     )
 }
 
-export default LibraryList
+const mapStateToProps = state => {
+    return { libraries: state.libraries }
+}
+
+export default connect(mapStateToProps)(LibraryList)
