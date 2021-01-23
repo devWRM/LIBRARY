222ebe4cbf5172a646ec91c4fda47b902ce51748
devWRM <mcwen@comcast.net>
2021-01-23 06:19:52 -0500

    created and set up to display in App.js

---
+++ frontend-library/src/components/libraryComponents/LibraryContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class LibraryContainer extends Component {
+    render() {
+        return (
+            <div>
+                Library Container
+            </div>
+        );
+    };
+}
+
+
+export default LibraryContainer;
