c79e5510cf6c76a0fa7f614d6e2d5f3e64280fc7
devWRM <mcwen@comcast.net>
2021-01-23 21:07:07 -0500

    added DELETE_LIBRARY case statement

--- frontend-library/src/reducers/librariesReducer.js
+++ frontend-library/src/reducers/librariesReducer.js
@@ -1,10 +1,28 @@
 export const librariesReducer = (state = [], action) => {
+
     switch(action.type) {
         case 'FETCH_LIBRARIES':
             return action.payload
         case 'NEW_LIBRARY':
             return [...state, action.payload]
+        case 'DELETE_LIBRARY':
+            let newLibraries = state.filter(library => library.id !== action.payload.id)
+            return newLibraries
         default:
             return state
     }
 }
+
+
+
+
+
+
+
+// let librariesArray = state.map(library => 
+//     if (library.id === action.payload.id) {
+//         return action.payload
+//     } else {
+//         return library
+//     }
+// })
