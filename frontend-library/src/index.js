634392beb5517051753308a35c689f224944741b
devWRM <mcwen@comcast.net>
2021-01-26 16:49:50 -0500

    uncommented BrowserRouter import & wrapped App in <Router> tags

--- frontend-library/src/index.js
+++ frontend-library/src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
@@ -22,9 +22,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-        
+        <Router>
             <App />
-        
+        </Router>
   </Provider>,
 
   document.getElementById('root')
