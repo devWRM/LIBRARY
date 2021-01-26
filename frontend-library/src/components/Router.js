9af373ac544d4d40eb33708822bc88ce23f1dd63
devWRM <mcwen@comcast.net>
2021-01-26 17:03:31 -0500

    created a Router file and made it a functional component; imported Switch & Route, Home, LibraryContainer

---
+++ frontend-library/src/components/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+import Home from './Home.js';
+import LibraryContainer from './LibraryContainer.js';
+
+
+const Router = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Router
