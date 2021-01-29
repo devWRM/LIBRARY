4897a5717e0b794929772960a8467e98415542f0
devWRM <mcwen@comcast.net>
2021-01-28 22:01:06 -0500

    used find to get the Library clicked out of the store & since comparing a string (params) to an integer (store library id) I used == as condition (NOT ===)

--- frontend-library/src/components/Library.js
+++ frontend-library/src/components/Library.js
@@ -5,20 +5,34 @@ import { connect } from 'react-redux';
 // import BookContainer from './BookContainer.js';
 
 const Library = (props) => {
-    // ID # of the Library name clicked             =>> props.match.params.id
+    // ID # of the Library name clicked             =>> props.match.params.id   ("7")
     // Array of Library objects in the Redux Store  =>> props.libraries
-    // ID # of the 1st Library in the Redux Store   =>> props.libraries[0].id
+    // ID # of the 1st Library in the Redux Store   =>> props.libraries[0].id   (7)
+    // NOTE Comparing a string "7" to an integer 7 so use == (NOT ===)
 
+        let libby = props.libraries.find(library =>                
+            library.id == props.match.params.id
+        )
 
     return (
         <div>
-            !!! LIBRARY !!! LIBRARY !!! LIBRARY !!!
             {/* <b>{props.library.name}</b><br></br>
             {props.library.street}<br></br>
             {props.library.city}<br></br>
             {props.library.state}<br></br>
 
             <BookContainer library={props.library} /> */}
+
+             
+
+            {libby.name}<br></br>
+            {libby.street}<br></br>
+            {libby.city}<br></br>
+            {libby.state}
+
+            
+
+
         </div>
     )
 }
