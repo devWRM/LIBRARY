05e3f7b483714c8faf8ee516733dd7f87eccf16c
devWRM <mcwen@comcast.net>
2021-01-28 22:48:11 -0500

    listed what Library App does and styled it

--- frontend-library/src/components/About.js
+++ frontend-library/src/components/About.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            ABOUT PAGE: ALL ABOUT LIBRARY APP!
+        <div
+        
+        style = {{
+            padding: '5rem'
+        }}
+        
+        >
+            <h3>ALL ABOUT LIBRARY APP!</h3>
+            <h4>Library App lets you:</h4>
+            <ul>
+                <li>see a list of libraries</li>
+                <li>create a library</li>
+                <li>delete a library (including all of its books)</li>
+                <li>see a list of books for a specific library</li>
+                <li>create a book for a specific library</li>
+                <li>delete a book</li>
+
+
+            </ul>
+
         </div>
     )
 }
