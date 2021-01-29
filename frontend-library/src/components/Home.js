5d2ad54203bd01277aa84d31c22d66bfb8042939
devWRM <mcwen@comcast.net>
2021-01-28 22:46:47 -0500

    created Welcome message and styled it

--- frontend-library/src/components/Home.js
+++ frontend-library/src/components/Home.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            HOME PAGE: WELCOME TO LIBRARY APP!
+        <div
+        
+        style = {{
+            // width: '50%',
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            fontSize: '30px',
+            
+            // border: '2px solid #ccc',
+            padding: '1rem'
+        }}
+        
+        >
+            <h1>WELCOME TO LIBRARY APP!</h1>
         </div>
     )
 }
